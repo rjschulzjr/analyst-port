07048af5bf6b700026492b66538f82378f191fe2
Ron Schulz <rjschulzjr@gmail.com>
2025-02-25 12:47:28 -0800

    Add files via upload

---
+++ Inner_Join_(Explicit Join) Example.sql
@@ -0,0 +1,13 @@
+/*INNER JOIN or EXPLICIT JOIN - optimizes query search by ignoring anything in the second table that does not have an entry in the first
+ (IE - drops records from the second table that are not in the first*/
+ 
+ select 
+ name, 
+ number, 
+ room, 
+ ssn, 
+ score 
+ 
+ from course inner join enrolls_in 
+ 
+ on course.number = enrolls_in.class;
---
+++ school_db_initial_example.sql
@@ -0,0 +1,5 @@
+CREATE database schooldb;
+USE schooldb;
+create table student (name varchar(20), city varchar(20));
+show tables;
+describe student;
